fae4900be422f2e9d70dbad4f0a191ba20738f14
szng <shezeneng@qq.com>
2024-07-30 22:54:13 +0800

    Exe3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body',
+    function (req, res) {
+        return JSON.stringify(req.body)
+    })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
@@ -80,7 +87,7 @@ app.post('/api/persons', (request, response) => {
         number: body.number
     }
     persons = persons.concat(person)
-    
+
     response.json(person)
 })
 
